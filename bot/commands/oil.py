38fbc42b7f2fec8d2398b35a77cadc66cfc24cbb
Hairetsu <Hairetsu@gmail.com>
2024-05-10 16:31:15 -0700

    update

--- bot/commands/oil.py
+++ bot/commands/oil.py
@@ -1,24 +1,14 @@
-
-
-
 import requests
 
+url = 'https://api.oilpriceapi.com/v1/prices/latest'
+headers = {
+  'Authorization': 'Token e7fe08fe4019dab013c50774620aef6f',
+  'Content-Type': 'application/json'
+}
+
+
+
 def oil_price():
-    # This is a placeholder URL and API key
-    api_url = "https://www.alphavantage.co/query?function=WTI&interval=daily&apikey=UEZCZ77OWPKJHA7J"
-
-
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    response = requests.get(api_url, headers=headers)
-    print(response.json())
-    if response.status_code == 200:
-        data = response.json()
-        # Assuming the API returns a JSON object and the price is stored under 'price'
-        oil_price = data
-        return oil_price
-    else:
-        return "Failed to fetch data"
-
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data['data']['price']
