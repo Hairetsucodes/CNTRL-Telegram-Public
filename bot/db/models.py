9113aa33ed047be2c64b71a37e249537b0bd9f2b
Hairetsu <Hairetsu@gmail.com>
2024-05-09 23:35:21 -0700

    update db stuff

--- bot/db/models.py
+++ bot/db/models.py
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(BIGINT, primary_key=True)
-    username = Column(String(50), nullable=False)
+    username = Column(String(100), nullable=False)
     blacklisted = Column(Integer, default=0)
 
 
@@ -17,7 +17,7 @@ class PrivateMessages(Base):
 
     __tablename__ = 'messages'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(50), nullable=False)
+    username = Column(String(100), nullable=False)
     userId = Column(BIGINT, ForeignKey('users.id'), nullable=False)
     message = Column(Text, nullable=False)
     createdAt = Column(DateTime, default=datetime.now)
@@ -27,7 +27,7 @@ class ChatMessages(Base):
     __tablename__ = 'chatMessages'
     id = Column(Integer, primary_key=True, autoincrement=True)
     chatId = Column(BIGINT, nullable=False)
-    username = Column(String(50), nullable=False)
+    username = Column(String(100), nullable=False)
     userId = Column(BIGINT, ForeignKey('users.id'), nullable=False)
     message = Column(Text, nullable=False)
     createdAt = Column(DateTime, default=datetime.now)
@@ -45,6 +45,8 @@ class UserWordCount(Base):
     __tablename__ = 'userWordCount'
     id = Column(Integer, primary_key=True, autoincrement=True)
     userId = Column(BIGINT, ForeignKey('users.id'), nullable=False)
+    username = Column(String(100), nullable=False)
+    chatId = Column(BIGINT, nullable=False)
     word = Column(String(50), nullable=False)
     count = Column(Integer, nullable=False)
     createdAt = Column(DateTime, default=datetime.now)
