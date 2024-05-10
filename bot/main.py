f37bf3b5fb6c0252e1c3c1efad7304cfd9e9c6ea
Hairetsu <Hairetsu@gmail.com>
2024-05-09 18:10:50 -0700

    update echo function > chat_logging

--- bot/main.py
+++ bot/main.py
@@ -47,19 +47,21 @@ async def ai(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(response)
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def chat_logging(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     if update.effective_user.username and update.effective_user.username != "None":
         logger.info(f"User {update.effective_user.username} sent a message: {update.message.text}")
     else:
         logger.info(f"User {update.effective_user.id} sent a message: {update.message.text}")
 
+
+
 def main() -> None:
     token = os.getenv("BOT_API_KEY")
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("ai", ai))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat_logging))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
