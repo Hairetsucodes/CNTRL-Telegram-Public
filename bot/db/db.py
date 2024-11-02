70937a44b836ac11d93d560708d796064339d505
Hairetsu <Hairetsu@gmail.com>
2024-11-02 18:50:41 -0400

    oppsies

--- bot/db/db.py
+++ bot/db/db.py
@@ -186,8 +186,8 @@ def last_five_messages(chatId, userId):
     db_session = SessionLocal()
     try:
         messages = db_session.query(ChatMessages).filter(ChatMessages.chatId == chatId, ChatMessages.userId == userId).all()[-5:]
-        logger.info(f"Last five messages: {messages}")
-        return [message.message for message in messages]
+        logger.info(f"{message.username}: {message.message}" for message in messages)
+        return [f"{message.username}: {message.message}" for message in messages]
     except Exception as e:
         print(f"An error occurred: {e}")
         db_session.rollback()
